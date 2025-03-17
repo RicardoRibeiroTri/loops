10e2ccbc53a8558f2f838511781aef1cab6de807
RicardoRibeiroTri <ricardoribeirotrindade273@gmail.com>
2025-03-17 10:03:24 -0300

    Add files via upload

---
+++ Contagem1a10.java
@@ -0,0 +1,20 @@
+package Loop;
+
+public class Contagem1a10 {
+
+	public static void main(String[] args) {
+		
+		
+		int i = 1;
+		System.out.println(i);
+		
+		while (i < 10) {
+	    i++;
+	    System.out.println(i);
+	    
+	    
+		}
+
+	}
+
+}
